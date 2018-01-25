098b73196a71f1768a89aa42411eb95ecc6f9961
ywCN <wangyouhao90@gmail.com>
2018-01-24 19:16:56 -0500

    install firebase and init it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyArwzscOXTvs4F0m_-4jX-xZOi9RVKJCcs',
+            authDomain: 'ssw695-18s.firebaseapp.com',
+            databaseURL: 'https://ssw695-18s.firebaseio.com',
+            projectId: 'ssw695-18s',
+            storageBucket: '',
+            messagingSenderId: '282335465798'
+        };
+        firebase.initializeApp(config);
+    }
     render() {
         return <div>Hello</div>;
     }
