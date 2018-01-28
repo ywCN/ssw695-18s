2657752126681422f60a0168b2494564b39dab12
ywCN <wangyouhao90@gmail.com>
2018-01-27 23:30:23 -0500

    render button based on auth state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 // TODO: import login and logout action and use them as the callbacks on login and logout buttons
 
@@ -7,37 +7,37 @@ class Header extends Component {
     // TODO: render a button, the rendered button is determined by the auth state
     renderContent() {
         if (this.props.auth) {
-            // return button with login callback
+            return <a onClick={() => console.log('login')}>login</a>;
         } else {
-            // return button with logout callback
+            return <a onClick={() => console.log('logout')}>logout</a>;
         }
     }
 
     render() {
         return (
-            //http://materializecss.com/navbar.html
             <nav>
                 <div className="nav-wrapper">
-                    <Link
-                        // to={this.props.auth ? '/surveys' : '/'}
-                        to={'/'}
-                        className="left brand-logo"
-                    >
+                    <Link to={'/'} className="left brand-logo">
                         CoderIn
                     </Link>
                     {/* <div className="right">{this.renderContent()}</div> */}
-                    <div className="right">{'login/logout'}</div>
+                    <Link
+                        to={'/myportfolio'}
+                        className="right"
+                        style={{ marginLeft: 30 }}
+                    >
+                        myportfolio
+                    </Link>
+                    <div className="right">{this.renderContent()}</div>
                 </div>
             </nav>
         );
     }
 }
-export default Header;
+// export default Header;
 
-// function mapStateToProps({ auth }) {
-//     return { auth };
-// }
+function mapStateToProps({ auth }) {
+    return { auth };
+}
 
-// TODO: subscribe to the auth state
-
-// export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header);
