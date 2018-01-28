2ae72913d18352472c44ec50bcecf00433322b96
ywCN <wangyouhao90@gmail.com>
2018-01-27 22:20:21 -0500

    add editPortfolio component

---
+++ src/components/EditPortfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// TODO: import savePortfolio action and use it in 'save' button
+
+const EditPortfolio = () => {
+    // TODO: render currentUser state in TextInput as init value
+    return (
+        <div>
+            EditPortfolio
+            {/* TODO: add a save button with savePortfolio action callback*/}
+        </div>
+    );
+};
+
+export default EditPortfolio;
