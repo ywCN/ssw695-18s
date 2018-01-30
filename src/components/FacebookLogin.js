6c9c729129c9f7393e5575131932521318c6fdeb
ywCN <wangyouhao90@gmail.com>
2018-01-30 15:54:08 -0500

    use action

--- src/components/FacebookLogin.js
+++ src/components/FacebookLogin.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import FacebookLogin from 'react-facebook-login';
+
+import { login } from '../actions';
 
 class MyComponent extends React.Component {
     responseFacebook(response) {
@@ -10,13 +13,15 @@ class MyComponent extends React.Component {
         return (
             <FacebookLogin
                 appId="164069247570253"
-                autoLoad={true}
+                autoLoad={false}
                 fields="name,email,picture"
                 scope="public_profile,user_friends,user_actions.books"
-                callback={this.responseFacebook}
+                // callback={this.responseFacebook}
+                callback={this.props.login(response)}
+                size="small"
             />
         );
     }
 }
 
-export default MyComponent;
+export default connect(null, { login })(MyComponent);
