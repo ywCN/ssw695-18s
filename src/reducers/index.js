8de6edf788c9336846bd3d11369905bd0f93c477
ywCN <wangyouhao90@gmail.com>
2018-01-30 16:08:20 -0500

    rename

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducers';
 
 const rootReducer = combineReducers({
-    auth: authReducer
+    user: authReducer
 });
 
 export default rootReducer;
