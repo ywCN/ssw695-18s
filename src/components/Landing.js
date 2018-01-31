edff475f99559d85a8df804691826c6535192a2a
ywCN <wangyouhao90@gmail.com>
2018-01-30 19:21:10 -0500

    show login on Landing and Logout page based on current user state

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import FacebookLogin from './FacebookLogin';
 
 const Landing = () => {
     return (
         <div style={{ textAlign: 'center' }}>
-            <h1>CoderIn!</h1>
-            Find smartest coders!
+            <div>
+                <h1>CoderIn!</h1>
+                Find smartest coders!
+            </div>
+            <FacebookLogin />
         </div>
     );
 };
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import FacebookLogin from './FacebookLogin';
 
 const Logout = () => {
     return (
         <div style={{ textAlign: 'center' }}>
             <h1>Thank you for using CoderIn!</h1>
+            <FacebookLogin />
         </div>
     );
 };
