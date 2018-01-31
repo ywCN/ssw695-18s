90cb2ccf09adacf4854ae1c82c7f4aa6dd15580c
ywCN <wangyouhao90@gmail.com>
2018-01-30 19:37:04 -0500

    clear the user props in store

--- src/reducers/authReducers.js
+++ src/reducers/authReducers.js
@@ -1,9 +1,12 @@
-import { LOGIN } from '../actions/types';
+import { LOGIN, LOGOUT } from '../actions/types';
 
 export default function(state = null, action) {
     switch (action.type) {
         case LOGIN:
             return { ...state, ...action.payload };
+        case LOGOUT:
+            console.log('logout reducer invoked');
+            return null;
         default:
             return state;
     }
