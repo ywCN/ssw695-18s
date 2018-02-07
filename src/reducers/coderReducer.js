de9ce1b35feed0d9fb601d6ae08933820eb1c5b6
ywCN <wangyouhao90@gmail.com>
2018-02-07 17:17:18 -0500

    rm comment

--- src/reducers/coderReducer.js
+++ src/reducers/coderReducer.js
@@ -3,14 +3,6 @@ import { FETCH_CODERS } from '../actions/types';
 export default function(state = null, action) {
     switch (action.type) {
         case FETCH_CODERS:
-            console.log('action.payload');
-            console.log('action.payload');
-            console.log('action.payload');
-            console.log('action.payload');
-
-            console.log(action.payload);
-            console.log(action.payload);
-            console.log(action.payload);
             return action.payload;
         default:
             return state;
