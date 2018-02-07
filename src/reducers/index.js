8fa0301ae53811b41c7d05dd5c136abb191e352f
ywCN <wangyouhao90@gmail.com>
2018-02-07 17:00:26 -0500

    add reducers for fetching coders and user portfolio

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducers';
+import coderReducer from './authReducers';
+import portfolioReducer from './portfolioReducer';
 
 const rootReducer = combineReducers({
-    user: authReducer
+    user: authReducer,
+    coders: coderReducer,
+    user_portfolio: portfolioReducer
 });
 
 export default rootReducer;
