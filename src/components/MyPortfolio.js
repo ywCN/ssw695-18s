58c1563ebb97717bde02fe6742332e4063d8c32a
ywCN <wangyouhao90@gmail.com>
2018-02-08 14:09:56 -0500

    fix reducer

--- src/components/MyPortfolio.js
+++ src/components/MyPortfolio.js
@@ -1,41 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
-// TODO: import fetchPortfolio action and use it in ComponentWillMount
 
-const MyPortfolio = () => {
-    // TODO: fetch current user info and display info
-    // can just display name and email for now
-    return (
-        <div>
-            {/* Dummy text */}
-            <div>MyPortfolio</div>
-            <div>MyPortfolio</div>
-            <div>MyPortfolio</div>
-            <div>MyPortfolio</div>
-            <div>MyPortfolio</div>
-            {/* Dummy text */}
+import { fetchPortfolio } from '../actions';
 
+class MyPortfolio extends Component {
+    componentWillMount() {
+        this.props.fetchPortfolio();
+    }
+    render() {
+        console.log(this.props);
+        console.log(this.props);
+        console.log(this.props);
+        console.log(this.props);
+        console.log(this.props);
+        return (
             <div>
-                <Button
-                    primary
-                    as={Link}
-                    to="/editportfolio"
-                    onClick={() => console.log('Edit Portfolio clicked')}
-                >
-                    Edit Portfolio
-                </Button>
-                <Button
-                    secondary
-                    as={Link}
-                    to="/addproject"
-                    onClick={() => console.log('Add Project clicked')}
-                >
-                    Add Project
-                </Button>
+                {/* Dummy text */}
+                <div>MyPortfolio</div>
+                <div>MyPortfolio</div>
+                <div>MyPortfolio</div>
+                <div>MyPortfolio</div>
+                <div>MyPortfolio</div>
+                {/* Dummy text */}
+
+                <div>
+                    <Button
+                        primary
+                        as={Link}
+                        to="/editportfolio"
+                        onClick={() => console.log('Edit Portfolio clicked')}
+                    >
+                        Edit Portfolio
+                    </Button>
+                    <Button
+                        secondary
+                        as={Link}
+                        to="/addproject"
+                        onClick={() => console.log('Add Project clicked')}
+                    >
+                        Add Project
+                    </Button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+}
+
+const mapStateToProps = ({ current_user_portfolio }) => {
+    return { current_user_portfolio };
 };
 
-export default MyPortfolio;
+export default connect(mapStateToProps, { fetchPortfolio })(MyPortfolio);
