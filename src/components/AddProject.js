210e51cca61f5eeae6d42e6d0b9b4e62e6c1e34c
ywCN <wangyouhao90@gmail.com>
2018-02-08 16:11:19 -0500

    roll backRevert "install semantic UI"
    This reverts commit 0e9ef82945e9deddf89c9abcb22eb7d0490e16ef.

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,55 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Icon, Form } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
 const AddProject = () => {
     return (
         <div>
-            <Form>
-                <Form.Group widths="equal">
-                    <Form.Field label="An HTML <input>" control="input" />
-                    <Form.Field label="An HTML <select>" control="select">
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                    </Form.Field>
-                </Form.Group>
-                <Form.Group grouped>
-                    <label>HTML radios</label>
-                    <Form.Field
-                        label="This one"
-                        control="input"
-                        type="radio"
-                        name="htmlRadios"
-                    />
-                    <Form.Field
-                        label="That one"
-                        control="input"
-                        type="radio"
-                        name="htmlRadios"
-                    />
-                </Form.Group>
-                <Form.Group grouped>
-                    <label>HTML checkboxes</label>
-                    <Form.Field
-                        label="This one"
-                        control="input"
-                        type="checkbox"
-                    />
-                    <Form.Field
-                        label="That one"
-                        control="input"
-                        type="checkbox"
-                    />
-                </Form.Group>
-                <Form.Field
-                    label="An HTML <textarea>"
-                    control="textarea"
-                    rows="3"
-                />
-                <Form.Field label="An HTML <button>" control="button">
-                    HTML Button
-                </Form.Field>
-            </Form>
+            <div>AddProject</div>
             <Button
                 animated
                 as={Link}
