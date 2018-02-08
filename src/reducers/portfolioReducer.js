4c0e5b2a1a2c289a89a5b0eb7408f81bc9b90cbc
ywCN <wangyouhao90@gmail.com>
2018-02-08 14:10:26 -0500

    fix reducer

--- src/reducers/portfolioReducer.js
+++ src/reducers/portfolioReducer.js
@@ -3,7 +3,7 @@ import { FETCH_PORTFOLIO } from '../actions/types';
 export default function(state = null, action) {
     switch (action.type) {
         case FETCH_PORTFOLIO:
-            return { ...state, ...action.payload };
+            return action.payload;
         default:
             return state;
     }
