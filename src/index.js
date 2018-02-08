b112a9bde6a60c06fa8f5b46b25136f7c88c7b23
ywCN <wangyouhao90@gmail.com>
2018-02-08 15:42:47 -0500

    install semantic-ui-css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import 'materialize-css/dist/css/materialize.min.css';
+import 'semantic-ui-css/semantic.min.css';
 import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 
