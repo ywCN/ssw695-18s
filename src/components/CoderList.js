67cdf7f0d0bc615c29abaac4b018629911b9cd3d
ywCN <wangyouhao90@gmail.com>
2018-02-08 15:11:26 -0500

    render cards in CoderList

--- src/components/CoderList.js
+++ src/components/CoderList.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Button, Card } from 'semantic-ui-react';
+
 import { fetchCoders } from './../actions';
 
 class CoderList extends Component {
     componentWillMount() {
         this.props.fetchCoders();
     }
-    renderCoders() {
+
+    renderCard() {
+        console.log(this.props.coders);
+        console.log(this.props.coders);
+        console.log(this.props.coders);
+
+        return this.props.coders.map(coder => {
+            // TODO: use id instead of name
+
+            const { name, role, exp } = coder;
+            return (
+                <Card key={name}>
+                    <Card.Content>
+                        <Card.Header>{name}</Card.Header>
+                        <Card.Meta>{role}</Card.Meta>
+                        <Card.Description>{exp}</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <div className="ui two buttons">
+                            {/* TODO: as={Link} */}
+                            <Button basic color="green">
+                                Portfolio
+                            </Button>
+                            <Button basic color="red">
+                                Ignore
+                            </Button>
+                        </div>
+                    </Card.Content>
+                </Card>
+            );
+        });
+    }
+    renderCards() {
         if (this.props.coders) {
-            return this.props.coders.map(coder => {
-                // TODO: use coder.id instead of coder
-                return <div key={coder}>coder</div>;
-            });
+            return <Card.Group>{this.renderCard()}</Card.Group>;
         } else {
-            console.log(this.props.coders);
             return <div />;
         }
     }
     render() {
-        return <div>{this.renderCoders()}</div>;
+        return <div>{this.renderCards()}</div>;
     }
 }
 
