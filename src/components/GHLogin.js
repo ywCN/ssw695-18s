e13edfa57b49cb9a35ef24739cf3880f30ab173b
ywCN <wangyouhao90@gmail.com>
2018-02-10 16:51:47 -0500

    add both dev and prod OD

--- src/components/GHLogin.js
+++ src/components/GHLogin.js
@@ -7,14 +7,18 @@ import GitHubLogin from 'react-github-login';
 class GHLogin extends React.Component {
     render() {
         const onSuccess = response => console.log(response);
-        const onFailure = response => console.error(response);
+        // const onFailure = response => console.error(response);
         if (!this.props.user) {
+            const currentClientId =
+                process.env.NODE_ENV === 'development'
+                    ? 'f76940e82ae1b9c20108'
+                    : 'e4f29a993da09ec310cf';
             return (
                 <GitHubLogin
                     redirectUri=""
-                    clientId="e4f29a993da09ec310cf"
+                    clientId={currentClientId}
                     onSuccess={onSuccess}
-                    onFailure={onFailure}
+                    // onFailure={onFailure}
                 />
             );
         } else {
