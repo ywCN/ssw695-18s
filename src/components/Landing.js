0c3232962c47e88929037ef1587531da54f29abf
ywCN <wangyouhao90@gmail.com>
2018-02-10 16:30:55 -0500

    abondoned GG login

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Segment, Button, Divider } from 'semantic-ui-react';
 
 import FBLogin from './FBLogin';
+// import GGLogin from './GGLogin';
+// import LILogin from './LILogin';
+// import GHLogin from './GHLogin';
 
 const Landing = () => {
     return (
@@ -10,13 +13,13 @@ const Landing = () => {
                 <h1>CoderIn!</h1>
                 Find smartest coders!
             </div>
-            <FBLogin />
             <Segment padded>
-                <Button primary fluid>
-                    <FBLogin />
-                </Button>
+                <Button as={FBLogin} color="blue" fluid />
                 <Divider horizontal>Or</Divider>
-                <Button secondary fluid>
+                {/* <Button color="red" fluid> */}
+                {/* </Button> */}
+                {/* <Divider horizontal>Or</Divider> */}
+                <Button color="black" fluid>
                     Sign Up Now
                 </Button>
             </Segment>
