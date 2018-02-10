1de2b8b9baa030e2415be6faf5ec3b7151c6680c
ywCN <wangyouhao90@gmail.com>
2018-02-10 18:07:31 -0500

    add log for testing

--- src/components/GHLogin.js
+++ src/components/GHLogin.js
@@ -7,18 +7,20 @@ import GitHubLogin from 'react-github-login';
 class GHLogin extends React.Component {
     render() {
         const onSuccess = response => console.log(response);
-        // const onFailure = response => console.error(response);
+        const onFailure = response => console.error(response);
         if (!this.props.user) {
             const currentClientId =
                 process.env.NODE_ENV === 'development'
                     ? 'f76940e82ae1b9c20108'
                     : 'e4f29a993da09ec310cf';
+            console.log('current environment is', process.env.NODE_ENV);
+            console.log('current client id is', currentClientId);
             return (
                 <GitHubLogin
                     redirectUri=""
                     clientId={currentClientId}
                     onSuccess={onSuccess}
-                    // onFailure={onFailure}
+                    onFailure={onFailure}
                 />
             );
         } else {
