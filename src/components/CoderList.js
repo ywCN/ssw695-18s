c3932999d59ebc58542900db118b6f99522429ed
ywCN <wangyouhao90@gmail.com>
2018-02-13 23:47:28 -0500

    added ModalView

--- src/components/CoderList.js
+++ src/components/CoderList.js
@@ -1,18 +1,117 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Button, Card } from 'semantic-ui-react';
+import {
+    Button,
+    Header,
+    Icon,
+    Modal,
+    Card,
+    Form,
+    Divider
+} from 'semantic-ui-react';
 
 import {
     fetchCoders,
     setCurrentCoderPortfolio,
     addContact
 } from './../actions';
-import CoderModal from './CoderModal';
 
 class CoderList extends Component {
     componentWillMount() {
         this.props.fetchCoders();
+    }
+    renderPortfolio = coder => {
+        const { name, email } = coder;
+        return (
+            <Form>
+                <Form.Group unstackable widths={2}>
+                    <Form.Input
+                        label="First name"
+                        defaultValue={name}
+                        readOnly
+                    />
+                    <Form.Input
+                        label="Last name"
+                        defaultValue="Last name"
+                        readOnly
+                    />
+                </Form.Group>
+                <Form.Group widths={2}>
+                    <Form.Input label="Email" defaultValue={email} readOnly />
+                    <Form.Input label="Phone" defaultValue="Phone" readOnly />
+                </Form.Group>
+            </Form>
+        );
+    };
+    renderProjectsHelper = coder => {
+        const { projects } = coder;
+        return projects.map(project => {
+            const { projectName, projectDescription } = project;
+            return (
+                // TODO: use id for key
+                <Form key={projectName}>
+                    <Form.Input
+                        label="Project Name"
+                        defaultValue={projectName}
+                        readOnly
+                    />
+                    <Form.TextArea
+                        label="Project Description"
+                        defaultValue={projectDescription}
+                        readOnly
+                    />
+                    <Divider section />
+                </Form>
+            );
+        });
+    };
+
+    renderProjects = coder => {
+        return <div>{this.renderProjectsHelper(coder)}</div>;
+    };
+
+    renderScrollContent = coder => {
+        return (
+            <div>
+                <Divider horizontal>Portfolio</Divider>
+                {this.renderPortfolio(coder)}
+                <Divider horizontal>Projects</Divider>
+                {this.renderProjects(coder)}
+            </div>
+        );
+    };
+
+    renderModal(coder) {
+        return (
+            <Modal
+                trigger={
+                    <Button basic color="green">
+                        View Portfolio
+                    </Button>
+                }
+            >
+                <Modal.Header>Profile Picture</Modal.Header>
+                <Modal.Content scrolling>
+                    <Modal.Description>
+                        <Header>Modal Header</Header>
+                        <p>
+                            This is an example of expanded content that will
+                            cause the modal's dimmer to scroll
+                        </p>
+
+                        <div>{this.renderScrollContent(coder)}</div>
+                    </Modal.Description>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button secondary>
+                        Exit <Icon name="left chevron" />
+                    </Button>
+                    <Button primary>
+                        Add Contact <Icon name="right chevron" />
+                    </Button>
+                </Modal.Actions>
+            </Modal>
+        );
     }
 
     renderCard() {
@@ -28,15 +127,8 @@ class CoderList extends Component {
                     </Card.Content>
                     <Card.Content extra>
                         <div className="ui two buttons">
-                            <CoderModal
-                                as={Link}
-                                to="/coderInfo"
-                                onClick={() =>
-                                    this.props.setCurrentCoderPortfolio(coder)
-                                }
-                            />
+                            {this.renderModal(coder)}
 
-                            {/* TODO: callback for addContact action */}
                             <Button
                                 basic
                                 color="blue"
