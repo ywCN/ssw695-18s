71406a0b731baa3ef6b5a0c2279fe0c72f5d977c
ywCN <wangyouhao90@gmail.com>
2018-03-26 11:11:52 -0400

    add icon and pic

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,63 +1,37 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Header, Segment } from 'semantic-ui-react';
+import { Header, Segment, Icon, Image } from 'semantic-ui-react';
 import { logout } from '../actions';
 
 class AppHeader extends React.Component {
-  renderHeaderLeft() {
-    // TODO: add greeting next to Logo
-    if (this.props.user) {
-      return (
-        <Header
-          as={Link}
-          floated="left"
-          to="/dashboard"
-          onClick={() => console.log('logo clicked while logged in')}
-        >
-          CodeIn
-        </Header>
-      );
-    } else {
-      return (
-        <Header
-          as={Link}
-          floated="left"
-          to="/"
-          onClick={() => console.log('logo clicked while logged out')}
-        >
-          CodeIn
-        </Header>
-      );
-    }
-  }
-  renderHeaderRight() {
-    if (this.props.user) {
-      return (
-        <div>
-          <Header as={Link} to="/myportfolio" floated="right">
-            myportfolio
-          </Header>
-          <Header
-            as={Link}
-            to="/logout"
-            floated="right"
-            onClick={() => {
-              this.props.logout();
-              console.log('logout clicked');
-            }}
-          >
-            logout
-          </Header>
-        </div>
-      );
-    }
-  }
   render() {
+    const logoPath = this.props.user ? '/dashboard' : '/';
     return (
       <Segment clearing color="blue">
-        {this.renderHeaderLeft()}
-        {this.renderHeaderRight()}
+        <Image
+          as={Link}
+          floated="left"
+          to={logoPath}
+          onClick={() => console.log('logo clicked while logged in')}
+          src="https://i.imgur.com/MVHIKpL.png"
+          size="small"
+        />
+
+        <Header as={Link} to="/myportfolio" floated="right">
+          <Icon name="user" size="large" />
+        </Header>
+        <Header
+          as={Link}
+          to="/logout"
+          floated="right"
+          onClick={() => {
+            this.props.logout();
+            console.log('logout clicked');
+          }}
+        >
+          <Icon name="log out" size="large" />
+        </Header>
       </Segment>
     );
   }
