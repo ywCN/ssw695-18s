75065ce67e7314801202acdee82e20149225878e
ywCN <wangyouhao90@gmail.com>
2018-03-27 21:52:35 -0400

    removed wrong style

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import faker from 'faker';
 import React, { Component } from 'react';
-import { Search, Grid, Header } from 'semantic-ui-react';
+import { Search, Grid } from 'semantic-ui-react';
 
 const source = _.times(5, () => ({
   title: faker.company.companyName(),
@@ -41,7 +41,7 @@ export default class SearchBar extends Component {
     const { isLoading, value, results } = this.state;
 
     return (
-      <Grid fluid>
+      <Grid>
         <Grid.Column width={8}>
           <Search
             loading={isLoading}
