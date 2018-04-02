e1992ed49b3298bb0a23d8f6be64cd4c5b631214
ywCN <wangyouhao90@gmail.com>
2018-04-02 19:30:09 -0400

    update button theme

--- src/components/EditProject.js
+++ src/components/EditProject.js
@@ -24,26 +24,24 @@ class EditProject extends Component {
             label="Project Description"
             defaultValue={projectDescription}
           />
-          <div>
-            <Button
-              primary
-              animated
-              as={Link}
-              to="/myportfolio"
-              onClick={() => console.log('add clicked')}
-            >
-              <Button.Content visible>Save</Button.Content>
-              <Button.Content hidden>
-                <Icon name="check" />
-              </Button.Content>
-            </Button>
-            <Button secondary animated as={Link} to="/myportfolio">
-              <Button.Content visible>Cancel</Button.Content>
-              <Button.Content hidden>
-                <Icon name="cancel" />
-              </Button.Content>
-            </Button>
-          </div>
+          <Button
+            basic
+            color="yellow"
+            as={Link}
+            to="/myportfolio"
+            onClick={() => console.log('add clicked')}
+          >
+            Save
+          </Button>
+          <Button
+            basic
+            color="red"
+            as={Link}
+            to="/myportfolio"
+            onClick={() => console.log('cancel clicked')}
+          >
+            Cancel
+          </Button>
         </Form>
         // TODO: add saveProject action callback
       );
