673ba5f4f4fe191e5841a1b9efe33ab1a5e9a7d2
ywCN <wangyouhao90@gmail.com>
2018-04-10 11:27:59 -0400

    render icons when login

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -16,18 +16,23 @@ class AppHeader extends React.Component {
   render() {
     const cookies = new Cookies();
     const logoPath = this.state.cookie || this.props.user ? '/dashboard' : '/';
-    const logoutIcon =
+    const Icons =
       this.state.cookie || this.props.user ? (
-        <Header
-          as={Link}
-          to="/logout"
-          floated="right"
-          onClick={() => {
-            this.props.logout(() => cookies.remove('loginStatus'));
-          }}
-        >
-          <Icon name="log out" size="large" color="grey" />
-        </Header>
+        <div>
+          <Header
+            as={Link}
+            to="/logout"
+            floated="right"
+            onClick={() => {
+              this.props.logout(() => cookies.remove('loginStatus'));
+            }}
+          >
+            <Icon name="log out" size="large" color="grey" />
+          </Header>
+          <Header as={Link} to="/myportfolio" floated="right">
+            <Icon name="user" size="large" color="grey" />
+          </Header>
+        </div>
       ) : null;
 
     const bigPic =
@@ -44,10 +49,7 @@ class AppHeader extends React.Component {
           src="https://i.imgur.com/WSUHc40.png"
           size="small"
         />
-        {logoutIcon}
-        <Header as={Link} to="/myportfolio" floated="right">
-          <Icon name="user" size="large" color="grey" />
-        </Header>
+        {Icons}
         <Header floated="right">
           <SearchBar />
         </Header>
