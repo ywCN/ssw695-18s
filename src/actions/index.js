1d731b07eaf9cadbd9793e5530be0ffaf0090381
ywCN <wangyouhao90@gmail.com>
2018-04-19 23:46:18 -0400

    clear localstorage to log out

--- src/actions/index.js
+++ src/actions/index.js
@@ -138,6 +138,7 @@ export const signUp = (
 export const logout = removeCookie => dispatch => {
   removeCookie();
   dispatch({ type: LOGOUT });
+  localStorage.clear();
 };
 
 export const fetchPortfolio = token => async dispatch => {
