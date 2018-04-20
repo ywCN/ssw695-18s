221783cc09d2693cc608630c5eaa1efc7c4695d7
ywCN <wangyouhao90@gmail.com>
2018-04-19 23:43:30 -0400

    applied persist

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
+import { PersistGate } from 'redux-persist/es/integration/react';
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './components/App';
-import reducers from './reducers';
+import configureStore from './store/configureStore';
 
-const store = createStore(reducers, applyMiddleware(thunk, logger));
+const { persistor, store } = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
---
+++ src/store/configureStore.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import reducers from './../reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default () => {
+  let store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
