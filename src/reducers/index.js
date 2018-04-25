4c5685c1f641406da7efd4e46ac65b3cccc3b0b1
ywCN <wangyouhao90@gmail.com>
2018-04-24 22:23:45 -0400

    combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import coderReducer from './coderReducer';
 import portfolioReducer from './portfolioReducer';
 import projectReducer from './projectReducer';
 import setCurrentCoderPortfolioReducer from './setCurrentCoderPortfolioReducer';
+import searchReducer from './searchReducer';
 
 const rootReducer = combineReducers({
   user: authReducer,
   coders: coderReducer,
   currentUserPortfolio: portfolioReducer,
   currentEdittingProject: projectReducer,
-  currentCoderPortfolio: setCurrentCoderPortfolioReducer
+  currentCoderPortfolio: setCurrentCoderPortfolioReducer,
+  searchResults: searchReducer
 });
 
 export default rootReducer;
